97cf382cb8027e84997347e645cccd3b222a7fe8
Oliver Martínez Novo <alu0100704129@ull.edu.es>
2014-12-08 20:24:15 +0000

    Subido archivo de pie de pagina

---
+++ pie.php
@@ -0,0 +1,12 @@
+   	<div class="container">
+      	<div class="row-clearfix">
+        	<div class="col-md-12 column">
+    			<hr>
+    			<footer>
+        			<p>&copy; Tecnologías de la Información para las Organizaciones</p>
+    			</footer>
+    		</div>
+    	</div>
+    </div>
+  </body>
+</html>
